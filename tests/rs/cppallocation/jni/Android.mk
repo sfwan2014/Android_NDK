19a806bd7e836feabfcc4dcb12046ac8a80d1063
sfwan <854347546@qq.com>
2014-11-06 17:25:39 +0800

    commit

---
+++ tests/rs/cppallocation/jni/Android.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	multiply.rs \
+	compute.cpp
+
+LOCAL_LDFLAGS += -L$(call host-path,$(TARGET_C_INCLUDES)/../lib/rs)
+
+LOCAL_LDLIBS := \
+	-lRS \
+	-lRScpp \
+	-lz \
+	-lcutils \
+	-lutils \
+	-lEGL \
+	-lGLESv1_CM \
+	-lGLESv2 \
+	-lui \
+	-lbcc \
+	-lbcinfo \
+	-lgui
+
+LOCAL_MODULE:= rstest-cppallocation
+
+LOCAL_C_INCLUDES += $(TARGET_C_INCLUDES)/rs/cpp
+LOCAL_C_INCLUDES += $(TARGET_C_INCLUDES)/rs
+LOCAL_C_INCLUDES += $(TARGET_OBJS)/$(LOCAL_MODULE)
+
+include $(BUILD_EXECUTABLE)
+
---
+++ tests/rs/cppbasic/jni/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	mono.rs \
+	compute.cpp
+
+LOCAL_LDFLAGS += -L$(call host-path,$(TARGET_C_INCLUDES)/../lib/rs)
+
+LOCAL_LDLIBS := \
+	-lz \
+	-lEGL \
+	-lGLESv1_CM \
+	-lGLESv2 \
+	-lui \
+	-lbcc \
+	-lbcinfo \
+	-lgui \
+	-ldl \
+	-lRScpp
+
+LOCAL_MODULE:= rstest-compute
+
+LOCAL_C_INCLUDES += $(TARGET_C_INCLUDES)/rs/cpp
+LOCAL_C_INCLUDES += $(TARGET_C_INCLUDES)/rs
+LOCAL_C_INCLUDES += $(TARGET_OBJS)/$(LOCAL_MODULE)
+
+include $(BUILD_EXECUTABLE)
+
